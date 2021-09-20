3223e7681f0a1fcb5a189c6440a68875e45e9560
Sasami Masaki Jurai <37247512+SasamiMasakiJurai@users.noreply.github.com>
2021-09-20 10:42:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-git clone https://github.com/fw876/helloworld.git package/helloworld
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
